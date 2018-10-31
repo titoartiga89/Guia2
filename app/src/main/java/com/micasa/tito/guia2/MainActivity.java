6b5ea2893b7bd0d5775419a9f3e66c65942c0353
titoartiga89 <42720521+titoartiga89@users.noreply.github.com>
2018-10-31 12:02:28 -0600

    Initial commit

---
+++ app/src/main/java/com/micasa/tito/guia2/MainActivity.java
@@ -0,0 +1,67 @@
+package com.micasa.tito.guia2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtURL;
+    private TextView lblEstado;
+    private Button btnDescargar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //inicializar
+        txtURL       = (EditText) findViewById(R.id.txtURL);
+        lblEstado    = (TextView) findViewById(R.id.lblEstado);
+        btnDescargar = (Button)   findViewById(R.id.btnDescargar);
+
+        //evento onClick
+        btnDescargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Descargar(
+                        MainActivity.this,
+                        lblEstado,
+                        btnDescargar
+                ).execute(txtURL.getText().toString());
+            }
+        });
+
+        verifyStoragePermissions(this);
+    }
+
+    //esto es para activar perimiso de escritura y lectura en versiones de android 6 en adelante
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    //persmission method.
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+}
